1333a5f2dd1b16a1f2c34b462657d4255233384d
Jose Escalona <JEscalona@lan.xven.com.ve>
2024-04-23 09:34:15 -0400

    new

--- contacto.php
+++ contacto.php
@@ -22,7 +22,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="/dashboard.html">Dashboard</a>
+                            <a class="nav-link" href="/index.html">Dashboard</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="/block.html">Block</a>
@@ -31,7 +31,7 @@
                             <a class="nav-link" href="/certificados.html">Certificados</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/contacto.html">Contacto</a>
+                            <a class="nav-link" href="/contacto.php">Contacto</a>
                         </li>
                         
                     </ul>
